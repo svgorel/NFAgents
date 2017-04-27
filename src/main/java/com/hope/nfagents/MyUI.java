cfd11101b3634dbcb727dad708c1068ddd6578d9
Orel <Orel@asu.intra.net>
2017-04-27 10:53:54 +0300

    фиксация версии 2.0

--- src/main/java/com/hope/nfagents/MyUI.java
+++ src/main/java/com/hope/nfagents/MyUI.java
@@ -290,34 +290,7 @@ public class MyUI extends UI {
         }
     }
 
-    
-    public static String readAll(Reader rd) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        int cp;
-        while ((cp = rd.read()) != -1) {
-            sb.append((char) cp);
-        }
-        return sb.toString();
-    }
-
-    
-    public static JSONArray readJsonFromUrl(String url) throws IOException {
-        // String s = URLEncoder.encode(url, "UTF-8");
-        // URL url = new URL(s);
-        InputStream is = new URL(url).openStream();
-        JSONArray json = null;
-        try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-            String jsonText = readAll(rd);
-            json = new JSONArray(jsonText);
-        } catch (JSONException e) {
-            Notification.show(e.getMessage(), Notification.Type.ERROR_MESSAGE);
-        } finally {
-            is.close();
-        }
-        return json;
-    }
-    
+   
     
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
